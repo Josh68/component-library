04b6bee24f13af8fbd44e75ae9eb3145208cd6ca
Josh Schneider <josh68@gmail.com>
2017-02-07 20:19:27 -0800

    initial, and completely unusable, version of a map

---
+++ stories/Map.story.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+const position = [51.505, -0.09];
+
+const style =  {
+  height: '400px',
+  width: '100%'
+};
+
+
+export default () => storiesOf('Map visualization')
+  .addWithInfo(
+    'simple usage',
+    'this is just a damned map!',
+    () => {
+      return (
+        <div style={{ display: 'flex', justifyContent: 'space-around', margin: '10% auto' }} >
+          <Map center={position} zoom={13}>
+            <TileLayer
+              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+              <Popup>
+                <span>A pretty CSS3 popup.<br />Easily customizable.</span>
+              </Popup>
+            </Marker>
+          </Map>
+        </div>
+      );
+    });
